28ccc94389dd906bf9df10c4fac505f1c8e92216
Mariusz Jakoniuk <jarmuszz@tuta.io>
2024-06-10 09:48:38 +0200

    emacs: Move to `org-ql`
    Signed-off-by: Mariusz Jakoniuk <jarmuszz@tuta.io>

--- home-manager/programs/emacs/config/init.el
+++ home-manager/programs/emacs/config/init.el
@@ -27,7 +27,10 @@
   (evil-mode 1)
   )
 
-(use-package magit)
+(use-package magit
+  :config
+  (evil-define-key 'normal 'global (kbd "SPC M") #'magit)
+  )
 
 (use-package projectile)
 
@@ -49,48 +52,80 @@
   :config
   (add-hook 'org-mode-hook #'org-modern-mode)
   (add-hook 'org-agenda-finalize-hook #'org-modern-agenda)
+  :custom
+  (org-modern-todo-faces
+   '(("IFTIME"   :background "turquoise"  :foreground "black")
+     ("WAITING"  :background "pale green" :foreground "black")
+     ("ONHOLD"   :background "light blue" :foreground "black")
+     ))
   )
 
-(defconst *org-directory* (file-truename "~/Sync/Org"))
+(defconst *org-directory* (file-truename "~/docs/Org"))
 
 (use-package org
   :config
   (add-hook 'org-mode-hook #'company-mode)
   (evil-define-key 'normal 'global
-    (kbd "SPC o f") #'org-agenda-visit-file
-    (kbd "SPC o a") #'org-agenda-list)
+    (kbd "SPC o a") #'org-agenda
+    (kbd "SPC C")   #'org-capture)
   :custom
-  (org-hide-leading-stars t)
-  (org-startup-with-inline-images t)
+  (org-agenda-files (list *org-directory*))
+  (org-agenda-span 14)
+  (org-agenda-prefix-format
+   '((agenda . " %i %-12:c%?-12t% s")
+    (todo . " %i %-12:c")
+    (tags . " %i %-12:c")
+    (search . " %i %-12:c")))
+  (org-directory *org-directory*)
   (org-hide-emphasis-markers t)
+  (org-hide-leading-stars t)
   (org-pretty-entities t)
-  (org-directory "~/Sync/Org")
-  (org-agenda-files '("~/Sync/Org/"))
+  (org-startup-with-inline-images t)
   (org-todo-keywords
-   '( "TODO" "EVENT" "IFTIME" "DONE"))
+   '( "TODO(t)" "IFTIME(i)" "WAITING(w)" "|" "ONHOLD(h)" "CANCELLED(c)" "DONE(d)"))
   (org-todo-keyword-faces
-   '(("EVENT"  . (:background "green" :foreground "white"))
-     ("IFTIME" . (:background "blue"  :foreground "white"))
+   '(("IFTIME"  . (:background "medium turquoise" :foreground "black"))
+     ("WAITING" . (:background "pale green"       :foreground "black"))
+     ("ONHOLD"  . (:background "light blue"       :foreground "black"))
      ))
-  )
 
-(use-package org-roam
-  :ensure t
-  :custom
-  (org-roam-directory (file-truename "~/Sync/Org"))
-  (org-roam-dailies-directory *org-directory*)
+  (org-capture-templates
+   `(("e" "Events" entry
+      (file ,(concat *org-directory* "/events.org"))
+      "* %?\nSCHEDULED: ")
+     ("a" "Agenda" entry
+      (file ,(concat *org-directory* "/agenda.org"))
+      "* %?\nSCHEDULED: ")
+     ("t" "Todo" entry
+      (file ,(concat *org-directory* "/agenda.org"))
+      "* TODO %?")
+     ("i" "Inbox" entry
+      (file ,(concat *org-directory* "/inbox.org"))
+      "* %?")
+     )))
+
+(use-package org-ql
   :config
   (evil-define-key 'normal 'global
-    (kbd "SPC O")   'org-roam-node-find
-    (kbd "SPC r c") 'org-roam-capture
-    (kbd "SPC r i") 'org-roam-node-insert
-    (kbd "SPC r 1") 'org-roam-dailies-goto-today
-    (kbd "SPC r d") 'org-roam-dailies-goto-date)
-  (setq org-roam-node-display-template
-	(concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
-  (org-roam-db-autosync-mode)
-  )
+    (kbd "SPC o o") 'org-ql-find-in-org-directory
+    (kbd "SPC o r") 'org-ql-view-recent-items
+    (kbd "SPC o s") 'org-ql-view-sidebar
+    (kbd "SPC o v") 'org-ql-view
+    ))
 
+(use-package deft
+  :custom
+  (deft-default-extension "org")
+  (deft-extensions '("org"))
+  (deft-directory *org-directory*)
+  (deft-recursive t)
+  (deft-text-mode 'org-mode)
+  (deft-use-filename-as-title t)
+  :config
+  (evil-define-key 'normal 'global
+    (kbd "SPC o d") #'deft)
+  )
+  
 ;; Backup files
 (setq backup-directory-alist `(("." . "~/.cache/emacs")))
 
@@ -109,3 +144,16 @@
 ;;; Nix
 (use-package nix-mode
   :mode "\\.nix\\'")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(deft yasnippet undo-tree treemacs-projectile treemacs-evil sbt-mode org-roam org-modern nix-mode neotree magit lsp-ui lsp-metals flycheck company color-theme-sanityinc-tomorrow base16-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
