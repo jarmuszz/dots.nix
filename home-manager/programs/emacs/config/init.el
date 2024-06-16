a9c1adcb8723f1358335185f9904ab8e046a61f2
Mariusz Jakoniuk <jarmuszz@tuta.io>
2024-06-16 17:33:07 +0200

    emacs: Move out org-mode configuration

--- home-manager/programs/emacs/config/init.el
+++ home-manager/programs/emacs/config/init.el
@@ -48,83 +48,6 @@
   :requires (treemacs projectile)
   )
 
-(use-package org-modern
-  :config
-  (add-hook 'org-mode-hook #'org-modern-mode)
-  (add-hook 'org-agenda-finalize-hook #'org-modern-agenda)
-  :custom
-  (org-modern-todo-faces
-   '(("IFTIME"   :background "turquoise"  :foreground "black")
-     ("WAITING"  :background "pale green" :foreground "black")
-     ("ONHOLD"   :background "light blue" :foreground "black")
-     ))
-  )
-
-(defconst *org-directory* (file-truename "~/docs/Org"))
-
-(use-package org
-  :config
-  (add-hook 'org-mode-hook #'company-mode)
-  (evil-define-key 'normal 'global
-    (kbd "SPC o a") #'org-agenda
-    (kbd "SPC C")   #'org-capture)
-  :custom
-  (org-agenda-files (list *org-directory*))
-  (org-agenda-span 14)
-  (org-agenda-prefix-format
-   '((agenda . " %i %-12:c%?-12t% s")
-    (todo . " %i %-12:c")
-    (tags . " %i %-12:c")
-    (search . " %i %-12:c")))
-  (org-directory *org-directory*)
-  (org-hide-emphasis-markers t)
-  (org-hide-leading-stars t)
-  (org-pretty-entities t)
-  (org-startup-with-inline-images t)
-  (org-todo-keywords
-   '( "TODO(t)" "IFTIME(i)" "WAITING(w)" "|" "ONHOLD(h)" "CANCELLED(c)" "DONE(d)"))
-  (org-todo-keyword-faces
-   '(("IFTIME"  . (:background "medium turquoise" :foreground "black"))
-     ("WAITING" . (:background "pale green"       :foreground "black"))
-     ("ONHOLD"  . (:background "light blue"       :foreground "black"))
-     ))
-
-  (org-capture-templates
-   `(("e" "Events" entry
-      (file ,(concat *org-directory* "/events.org"))
-      "* %?\nSCHEDULED: ")
-     ("a" "Agenda" entry
-      (file ,(concat *org-directory* "/agenda.org"))
-      "* %?\nSCHEDULED: ")
-     ("t" "Todo" entry
-      (file ,(concat *org-directory* "/agenda.org"))
-      "* TODO %?")
-     ("i" "Inbox" entry
-      (file ,(concat *org-directory* "/inbox.org"))
-      "* %?")
-     )))
-
-(use-package org-ql
-  :config
-  (evil-define-key 'normal 'global
-    (kbd "SPC o o") 'org-ql-find-in-org-directory
-    (kbd "SPC o r") 'org-ql-view-recent-items
-    (kbd "SPC o s") 'org-ql-view-sidebar
-    (kbd "SPC o v") 'org-ql-view
-    ))
-
-(use-package deft
-  :custom
-  (deft-default-extension "org")
-  (deft-extensions '("org"))
-  (deft-directory *org-directory*)
-  (deft-recursive t)
-  (deft-text-mode 'org-mode)
-  (deft-use-filename-as-title t)
-  :config
-  (evil-define-key 'normal 'global
-    (kbd "SPC o d") #'deft)
-  )
   
 ;; Backup files
 (setq backup-directory-alist `(("." . "~/.cache/emacs")))
@@ -135,8 +58,13 @@
 (when (display-graphic-p)
   (pixel-scroll-precision-mode))
 
+
 ;; Programming
 (add-to-list 'load-path "~/.config/emacs/lisp")
+
+;;; Org
+(require 'org-config)
+
 
 ;;; Scala
 (require 'scala)
@@ -150,7 +78,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(deft yasnippet undo-tree treemacs-projectile treemacs-evil sbt-mode org-roam org-modern nix-mode neotree magit lsp-ui lsp-metals flycheck company color-theme-sanityinc-tomorrow base16-theme)))
+   '(deft yasnippet undo-tree treemacs-projectile treemacs-evil sbt-mode org-roam org-modern nix-mode neotree magit lsp-ui lsp-metals flycheck company color-theme-sanityinc-tomorrow base16-theme))
+ '(warning-suppress-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ home-manager/programs/emacs/config/lisp/org-config.el
@@ -0,0 +1,79 @@
+(provide 'org-config)
+
+(defconst *org-directory* (file-truename "~/docs/Org"))
+
+(use-package org-modern
+  :config
+  (add-hook 'org-mode-hook #'org-modern-mode)
+  (add-hook 'org-agenda-finalize-hook #'org-modern-agenda)
+  :custom
+  (org-modern-todo-faces
+   '(("IFTIME"   :background "turquoise"  :foreground "black")
+     ("WAITING"  :background "pale green" :foreground "black")
+     ("ONHOLD"   :background "light blue" :foreground "black")
+     ))
+  )
+
+(use-package org
+  :config
+  (add-hook 'org-mode-hook #'company-mode)
+  (evil-define-key 'normal 'global
+    (kbd "SPC o a") #'org-agenda
+    (kbd "SPC C")   #'org-capture)
+  :custom
+  (org-agenda-files (list *org-directory*))
+  (org-agenda-span 14)
+  (org-agenda-prefix-format
+   '((agenda . " %i %-12:c%?-12t% s")
+    (todo    . " %i %-12:c")
+    (tags    . " %i %-12:c")
+    (search  . " %i %-12:c")))
+  (org-directory *org-directory*)
+  (org-elipsis "…")
+  (org-hide-emphasis-markers t)
+  (org-hide-leading-stars t)
+  (org-pretty-entities t)
+  (org-startup-with-inline-images t)
+  (org-todo-keywords
+   '( "TODO(t)" "IFTIME(i)" "WAITING(w)" "|" "ONHOLD(h)" "CANCELLED(c)" "DONE(d)"))
+  (org-todo-keyword-faces
+   '(("IFTIME"  . (:background "medium turquoise" :foreground "black"))
+     ("WAITING" . (:background "pale green"       :foreground "black"))
+     ("ONHOLD"  . (:background "light blue"       :foreground "black"))
+     ))
+
+  (org-capture-templates
+   `(("e" "Events" entry
+      (file ,(concat *org-directory* "/events.org"))
+      "* %?\nSCHEDULED: ")
+     ("a" "Agenda" entry
+      (file ,(concat *org-directory* "/agenda.org"))
+      "* %?\nSCHEDULED: ")
+     ("t" "Todo" entry
+      (file ,(concat *org-directory* "/agenda.org"))
+      "* TODO %?")
+     ("i" "Inbox" entry
+      (file ,(concat *org-directory* "/inbox.org"))
+      "* %?")
+     )))
+
+(use-package org-ql
+  :config
+  (evil-define-key 'normal 'global
+    (kbd "SPC o o") 'org-ql-find-in-org-directory
+    (kbd "SPC o r") 'org-ql-view-recent-items
+    (kbd "SPC o s") 'org-ql-view-sidebar
+    (kbd "SPC o v") 'org-ql-view
+    ))
+
+(use-package deft
+  :custom
+  (deft-default-extension "org")
+  (deft-extensions '("org"))
+  (deft-directory *org-directory*)
+  (deft-text-mode 'org-mode)
+  (deft-use-filename-as-title t)
+  :config
+  (evil-define-key 'normal 'global
+    (kbd "SPC o d") #'deft)
+  )
