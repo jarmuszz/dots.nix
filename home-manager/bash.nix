09044f580ee76b28286d1aeddc1ee76d905d28f0
Mariusz Jakoniuk <jarmuszz@fixpoint.dev>
2026-02-08 12:57:38 +0100

    Change bash prompt

--- home-manager/bash.nix
+++ home-manager/bash.nix
@@ -7,6 +7,7 @@
     shellAliases = {
       G = "grep -r";
       v = "vim";
+      ls = "ls -F";
       l = "ls -l";
       ll = "ls -l";
       ".." = "cd ..";
@@ -15,5 +16,17 @@
       sudo = "sudo ";
       userctl = "systemctl --user";
     };
+
+    bashrcExtra = '' # bash
+      prompt() {
+        if [ -n "''${IN_NIX_SHELL}" ]; then
+          PS1="(dev)% "
+        else
+          PS1="% "
+        fi
+      }
+
+      PROMPT_COMMAND=prompt
+    '';
   };
 }
