cfb27cdb42ad4b9130911e6b7775454f3385ac7e
Mariusz Jakoniuk <jarmuszz@fixpoint.dev>
2026-02-08 12:57:38 +0100

    Enable nixd

--- home-manager/nixvim/conf/lsp.nix
+++ home-manager/nixvim/conf/lsp.nix
@@ -4,11 +4,5 @@
 
   lsp.servers.pylsp.enable = true;
   lsp.servers.rust_analyzer.enable = true;
-  # plugins.lsp.servers.metals = {
-  #   enable = true;
-  #   package = pkgs.metals;
-  #   settings = {
-  #     init_options.statusBarProvider = "off";
-  #   };
-  # };
+  lsp.servers.nixd.enable = true;
 }
